e00b806734f820f6f1bfdb7c42bf8ca12a4c03ee
suha jin <89185836+djm07073@users.noreply.github.com>
2025-08-08 22:55:05 +0900

    fix(ante): consolidate balance checks into ValidateTransactionCosts
    - Merge VerifyAccountBalance and CanTransfer into single ValidateTransactionCosts function
    - Move core.Message creation before validation for efficiency
    - Keep fee deduction (ConsumeFeesAndEmitEvent) unchanged
    - Reduce duplicated balance validation logic

--- ante/evm/07_can_transfer.go
+++ ante/evm/07_can_transfer.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/params"
 
 	anteinterfaces "github.com/cosmos/evm/ante/interfaces"
 	"github.com/cosmos/evm/utils"
@@ -17,7 +18,85 @@ import (
 	errortypes "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
+// ValidateTransactionCosts checks that the sender has enough balance to cover both transaction costs and value transfer
+func ValidateTransactionCosts(
+	ctx sdk.Context,
+	evmKeeper anteinterfaces.EVMKeeper,
+	msg *core.Message,
+	txData evmtypes.TxData,
+	baseFee *big.Int,
+	rules params.Rules,
+) error {
+	// Check if max fee per gas is at least the block base fee (EIP-1559)
+	if rules.IsLondon && msg.GasFeeCap != nil && msg.GasFeeCap.Cmp(baseFee) < 0 {
+		return errorsmod.Wrapf(
+			errortypes.ErrInsufficientFee,
+			"max fee per gas less than block base fee (%s < %s)",
+			msg.GasFeeCap, baseFee,
+		)
+	}
+
+	// Get the sender's account balance
+	account := evmKeeper.GetAccount(ctx, msg.From)
+	if account == nil {
+		return errorsmod.Wrapf(
+			errortypes.ErrUnknownAddress,
+			"account %s does not exist",
+			msg.From.Hex(),
+		)
+	}
+
+	// Check that the transaction cost is positive
+	txCost := txData.Cost()
+	if txCost.Sign() < 0 {
+		return errorsmod.Wrapf(
+			errortypes.ErrInvalidCoins,
+			"tx cost (%s) is negative and invalid", txCost,
+		)
+	}
+
+	// Check that sender has enough balance to cover tx cost
+	// Convert balance to big.Int for comparison
+	balance := account.Balance.ToBig()
+	if balance.Cmp(txCost) < 0 {
+		return errorsmod.Wrapf(
+			errortypes.ErrInsufficientFunds,
+			"sender balance < tx cost (%s < %s)", balance, txCost,
+		)
+	}
+
+	// Additional check for value transfer using EVM state
+	if msg.Value.Sign() > 0 {
+		params := evmKeeper.GetParams(ctx)
+		cfg := &statedb.EVMConfig{
+			Params:   params,
+			CoinBase: common.Address{},
+			BaseFee:  baseFee,
+		}
+
+		stateDB := statedb.New(ctx, evmKeeper, statedb.NewEmptyTxConfig(common.BytesToHash(ctx.HeaderHash())))
+		evm := evmKeeper.NewEVM(ctx, *msg, cfg, evmtypes.NewNoOpTracer(), stateDB)
+
+		convertedValue, err := utils.Uint256FromBigInt(msg.Value)
+		if err != nil {
+			return err
+		}
+		
+		if !evm.Context.CanTransfer(stateDB, msg.From, convertedValue) {
+			return errorsmod.Wrapf(
+				errortypes.ErrInsufficientFunds,
+				"failed to transfer %s from address %s using the EVM block context transfer function",
+				msg.Value,
+				msg.From,
+			)
+		}
+	}
+
+	return nil
+}
+
 // CanTransfer checks if the sender is allowed to transfer funds according to the EVM block
+// Deprecated: Use ValidateTransactionCosts instead
 func CanTransfer(
 	ctx sdk.Context,
 	evmKeeper anteinterfaces.EVMKeeper,
